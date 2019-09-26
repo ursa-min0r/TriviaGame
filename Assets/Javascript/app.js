9ae71c7bb547d90f9bc532acf021aa22d5e8a04a
Polaris <tristynkcline@gmail.com>
2019-09-26 14:36:50 -0500

    Update Javascript

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,3 +1,58 @@
-var keanuQuestions = [];
+var questions = { 
+   
+    q1: {
+    keanuQuestion: "1) What was the title of Keanu's first breakout role in film?", 
+    keanuChoices: ["A) Bill & Ted's Bogus Adventure", "B) The Matrix", "C) Bill & Ted's Excellent Adventure", "D) Babes in Toyland"], 
+    keanuAnswer: "Answer: C) Bill & Ted's Excellent Adventure" 
+    }, 
+                        
+     
+   q2: {
+    keanuQuestion: "2) Which film starring Keanu was considered his most controversial?", 
+    keanuChoices: ["A) River's Edge", "B) My Own Private Idaho", "C) Brotherhood of Justice", "D) Siberia"], 
+    keanuAnswer: "Answer: A) River's Edge" 
+    },
 
-var keanuAnswers = [];
+     
+   q3: {
+    keanuQuestion: "3) Which word or saying is recognized as Keanu's most iconic line in cinema history?",
+    keanuChoices: ["A) Excellent", "B) Whoa", "C) I Know Kung Fu", "D) Yeah, I'm Thinking I'm Back"],
+    keanuAnswer: "Answer: B) Whoa"
+    },
+
+    q4: {
+    keanuQuestion: "4) Which former Co-Star does Keanu consider his best friend?",
+    keanuChoices: ["A) Crispin Glover", "B) Carrie-Anne Moss", "C) Sandra Bullock", " D) Keifer Sutherland"],
+    keanuAnswer: "Answer: C) Sandra Bullock"
+    },
+
+    q5: {
+    keanuQuestion: "5) Where was Keanu born?",
+    keanuChoices: ["A) Honolulu, Hawaii", "B) Bristol, England", "C) Toronto, Canada", "D) Beirut, Lebanon"],
+    keanuAnswer: "Answer: D) Beirut, Lebanon"
+    }
+    
+};
+
+var didYouKnow1 = ["Did you know? Keanu's mother was a Costume Designer for theatre and movies, and designed Dolly Parton's iconic bunny suit for her 1978 Playboy cover."]
+var didYouKnow2 = ["Did you know? Will Smith, Nicholas Cage, Brad Pitt, and Val Kilmer all turned down the role to play Neo in The Matrix."]   
+var didYouKnow3 = ["Did you know? Bill & Ted's Excellent Adventure was never intended to be the box office hit that it was, and the immediate success of the film took Keanu by surprise!"]   
+var didYouKnow4 = ["Did you know? Keanu turned down the offer to revise his role in Speed 2 to tour with his band, Dogstar, and to act in a Shakespere play in his hometown in Canada. "]   
+var didYouKnow5 = ["Did you know? Keanu started his own motorcycle company called ARCH Motorcycles, which specializes in producing custom made motorcycles for the consumer."]                       
+
+var playerScore = 0;
+
+var newDiv= $("<div>")
+newDiv.
+
+
+
+newDiv.appendTo($("#questionBody"))
+    
+
+    
+    console.log(questions.q1.keanuQuestion);
+    console.log(questions.q1.keanuChoices);
+
+
+    
